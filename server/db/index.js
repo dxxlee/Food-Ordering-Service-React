92da6df7bbbb20a612f775c5041ace154ab627ee
Daniel Li <149508426+dxxlee@users.noreply.github.com>
2025-02-07 12:34:45 +0600

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -2,15 +2,16 @@ require('dotenv').config();
 
 const mongoose = require("mongoose");
 
-const dbHost = process.env.DB_HOST || 'localhost';
-const dbPort = process.env.DB_PORT || 27017;
-const dbName = process.env.DB_NAME || 'food-ordering';
+//const dbHost = process.env.DB_HOST || 'localhost';
+//const dbPort = process.env.DB_PORT || 27017;
+//const dbName = process.env.DB_NAME || 'food-ordering';
 
-const dbUri = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+// const dbUri = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+const uri = process.env.MONGO_URI;
 
 const db = async () => {
   try {
-    await mongoose.connect(dbUri);
+    await mongoose.connect(uri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
